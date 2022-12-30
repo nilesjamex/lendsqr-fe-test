73cc09d0c27b0e5f9c2980bb34a288e817b12b87
Niles <egbaidomehjamez@gmail.com>
2022-12-30 19:06:30 +0100

    created Login page component

---
+++ src/Components/Login/index.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import styles from './Login.module.scss'
+// import images
+import Logo from '../../Assets/svg/logo.svg'
+import Pablo from '../../Assets/pablo2.svg'
+import { useNavigate } from 'react-router-dom'
+const Login = () => {
+    const getIn = React.useRef() as React.MutableRefObject<HTMLAnchorElement>;
+    useEffect(() => {
+        if (window.sessionStorage.getItem('user') === 'authenticated') {
+            navigate('/dashboard')
+          }
+    })
+    const navigate = useNavigate()
+    type User = {
+        email?: string;
+        password?: string
+    }
+    const [active, setActive] = useState<boolean>(false)
+    const [user, setUser] = useState<object>({
+        email: '',
+        password: ''
+    })
+    const {email, password}: User = user;
+    const onChange = (e: any) => {
+        const value = e.target.value
+        setUser({...user, [e.target.name]: value})
+    }
+    const onSubmit = (e: any) => {
+        e.preventDefault()
+        setUser({
+            email: '',
+            password: ''
+    })
+    window.sessionStorage.setItem('user', 'authenticated')
+    getIn?.current?.click()
+    navigate('/dashboard')
+    }
+  return (
+    <section className={styles.login}>
+        <div className={styles.login__logo}>
+            <img className={styles.logo} src={Logo} alt="" />
+            <div>
+              <img className={styles.pablo} width="600" height="338" src={Pablo} alt="" />
+            </div>
+            
+        </div>
+
+        <div className={styles.login__details}>
+            <div className={styles.login__details__intro}>
+            <h4>Welcome!</h4>
+            <p>Enter details to login.</p>
+            </div>
+            <form action="" onSubmit={onSubmit}>
+                <input type="email" placeholder='Email' value={email} name="email" onChange={onChange} required/>
+                <div className={styles.pass}>
+                <input type={active ? 'text' : 'password' } placeholder='Password' value={password} name="password" id="" onChange={onChange} required/>
+                <span onClick={() => {setActive(!active)}}>SHOW</span>
+                </div>
+                <p>Forgot PASSWORD?</p>
+                <a ref={getIn } href="/dashboard" style={{display: "none"}}></a>
+                <button type='submit'>LOG IN</button>
+            </form>
+        </div>
+    </section>
+  )
+}
+
+export default Login
